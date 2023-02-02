108f7a618ab94dbba71a3da504b86af57de8b0e8
Benezerds <benzzkrieg@gmail.com>
2023-02-03 02:36:30 +0700

    Added unit testing for login

---
+++ tests/UserModelTest.php
@@ -0,0 +1,23 @@
+<?php
+
+use Model\UserModel;
+
+require_once '../Model/UserModel.php';
+
+class UserModelTest extends PHPUnit\Framework\TestCase
+{
+    public function testLoginSuccess()
+    {
+        $userModel = new UserModel();
+
+        $_POST['UserID'] = '12';
+        $_POST['Password'] = 'ben123';
+
+        $result = $userModel->login();
+
+        $this->assertEquals(true, $result);
+        $this->assertArrayHasKey('UserID', $_SESSION);
+        $this->assertArrayHasKey('Password', $_SESSION);
+        $this->assertArrayHasKey('Type', $_SESSION);
+    }
+}
